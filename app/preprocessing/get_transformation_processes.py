16821c3190ee1627e4297983d5a44914bd62042c
MarcelSchi <155979217+MarcelSchi@users.noreply.github.com>
2025-04-30 16:51:18 +0200

    Update get_transformation_processes.py

--- app/preprocessing/get_transformation_processes.py
+++ app/preprocessing/get_transformation_processes.py
@@ -1,14 +1,17 @@
 from app.training.load_config import ConfigLoader
 from app.preprocessing.register_augmentation_strategies import Transformation_Registry, auto_register_transformations
 
-
+# function to search for all augmentation strategies that are defined in the app.augmentation directory
+# helps to dynamically add new strategies by registering them in a new registry
 class TransformationProcess:
     def __init__(self, config_loader: ConfigLoader, transformation_name):
         auto_register_transformations(module_path="app.augmentation")
 
         self.config = config_loader.get_config()
+        # ensures that a augmentation strategy is defined in the configuration file actually exists.  
         if transformation_name not in Transformation_Registry:
             available_transformations = ", ".join(Transformation_Registry.keys())
+            # If depicted string does not exist: Provide user a list of all existing augmentation strategies.
             raise ValueError(
                 f"Transformation '{transformation_name}' not registered. "
                 f"Please check all available transformations: {available_transformations}."
