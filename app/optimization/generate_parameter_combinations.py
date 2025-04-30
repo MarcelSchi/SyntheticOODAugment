03129f4f4046502c42fdf34a562b3c9fc0b32eec
MarcelSchi <155979217+MarcelSchi@users.noreply.github.com>
2025-04-30 16:53:52 +0200

    Update generate_parameter_combinations.py

--- app/optimization/generate_parameter_combinations.py
+++ app/optimization/generate_parameter_combinations.py
@@ -1,6 +1,6 @@
 from itertools import product
 
-
+# all potential parameter combinations are saved as a list, in a dictionary
 def generate_parameter_combinations(param_grid):
     parameter_combinations = list(product(*param_grid.values()))
     param_keys = list(param_grid.keys())
