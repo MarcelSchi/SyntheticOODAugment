2656ebd5d0b94d96e5cd57280a152624a7bfc601
MarcelSchi <155979217+MarcelSchi@users.noreply.github.com>
2025-05-01 10:56:54 +0200

    Update transform_operations.py

--- app/augmentation/transform_operations.py
+++ app/augmentation/transform_operations.py
@@ -6,12 +6,18 @@ from app.training.config_summary import Config_Summary
 
 
 class BaseImageTransform(TransformationStrategy):
+    """
+    This function depicts a basic transformation which applies all necessary preprocessing steps.
+    Every augmentation operation function can inherit from the BaseImageTransform() function.
+    This saves time and allows a modular, dynamic usage of a variety of augmentation strategies.
+    """
 
     def __init__(self, config: Config_Summary):
         super().__init__(config)
         self.grayscale_transform = transforms.Grayscale(
             num_output_channels=1) if config.grayscale else transforms.Lambda(lambda x: x)
 
+    # transformation steps are necessary for an appropriate image input
     def get_transformation_steps(self):
         transform_steps = [
             transforms.Resize((self.config.input_shape, self.config.input_shape)),
@@ -22,10 +28,13 @@ class BaseImageTransform(TransformationStrategy):
 
         return transform_steps
 
+    # abstract method can be used for inheritage by other functions. The base transformation 
+    # does not need any additional processing
     @abstractmethod
     def process_image(self, img: Image.Image) -> Image.Image:
         pass
 
+    # this function is used by all augmentation operations to chosse the depicted function in the config file
     def get_transform_operations(self):
         transform_steps = []
         transform_steps.append(transforms.Lambda(self.process_image))
