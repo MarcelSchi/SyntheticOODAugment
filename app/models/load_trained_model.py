54955039c0404a34312f9e3f3bc872773639379a
MarcelSchi <155979217+MarcelSchi@users.noreply.github.com>
2025-05-01 11:16:19 +0200

    Update load_trained_model.py

--- app/models/load_trained_model.py
+++ app/models/load_trained_model.py
@@ -2,6 +2,7 @@ from app.models.load_model import load_model
 import torch
 
 
+# already trained model, that is saved as .pth file, can be loaded to perform new evaluation
 def load_trained_model(path, num_classes=5, grayscale=False):
     model = load_model(num_classes=num_classes, grayscale=grayscale)
     model.load_state_dict(torch.load(path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
