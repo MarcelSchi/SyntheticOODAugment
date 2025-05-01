fa2b93c7d4a2cdf329a8adac4188fe613d78922a
MarcelSchi <155979217+MarcelSchi@users.noreply.github.com>
2025-05-01 11:19:34 +0200

    Update TransformationStrategy.py

--- app/preprocessing/TransformationStrategy.py
+++ app/preprocessing/TransformationStrategy.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 
+# Helps to define preprocessing steps via augmentation by inheriting from this function
 class TransformationStrategy(ABC):
     def __init__(self, config):
         self.config = config
